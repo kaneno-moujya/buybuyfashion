4a710b23a1443455a537c6faa8f0c080aebd5fab
TInoue <ublsap.3sao@gmail.com>
2025-03-24 17:39:24 +0900

    feat: pandacssの導入

---
+++ apps/web/panda.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  jsxFramework: "qwik",
+
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+  // Files to exclude
+  exclude: [],
+
+  globalCss: {
+    html: {
+      fontSize: "10px"
+    }
+  },
+
+  // Useful for theme customization
+  theme: {
+    tokens: {
+      colors: {},
+      fontSizes: {
+        s: {value: "3rem"}
+      }
+    },
+    breakpoints: {
+      sp: "0px",
+      pc: "749px",
+    },
+    extend: {},
+  },
+
+  // The output directory for your css system
+  outdir: "styled-system",
+});
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,15 +1,60 @@
-import { component$ } from "@builder.io/qwik";
+import {component$, useStore, $, useVisibleTask$, useTask$, useComputed$, useOnWindow} from "@builder.io/qwik";
+import {Link} from "@builder.io/qwik-city";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import {css} from "@styled-system/css";
 
-export default component$(() => {
+interface Props {
+  hoge?: number;
+}
+
+export default component$((props: Props) => {
+  const count = useStore({
+    number: 0
+  });
+
+  const style = css({
+    color: {
+      spOnly: "red",
+      pc: "blue"
+    },
+    fontSize: "s"
+  })
+
+  useOnWindow(["resize", "DOMContentLoaded"], $(() => {
+
+  }));
+
+  const hoge = useComputed$(() => {
+    return count.number + "秒";
+  })
+
+  const handleClick = $(() => {
+    count.number++;
+  })
+
+  useVisibleTask$(() => {
+    console.log("Mounted");
+  });
+
+  useTask$(({track, cleanup}) => {
+    track(() => count.number);
+    console.log("Count up");
+    cleanup(() => {
+      console.log("unmounted");
+    })
+  });
+
   return (
     <>
-      <h1>Hi 👋</h1>
+      <h1 class={style}>Hi 👋</h1>
       <div>
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
       </div>
+      <button type={"button"} onClick$={handleClick}>Count: {count.number}</button>
+      <p>{hoge.value}</p>
+      <Link>hoge</Link>
     </>
   );
 });
